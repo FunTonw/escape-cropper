28e524dddfe52d4476c96bffa494e6538c76e4cf
FunTonw <qw321888@gmail.com>
2022-06-02 16:41:22 +0800

    add: edit-button

--- all.js
+++ all.js
@@ -10,7 +10,41 @@ prevBtn.addEventListener("click", () => {
   carousel.scrollTo(0, 0);
 });
 
-
+// edit-button
+const editingButtonGroup = document.querySelector('.editing_button-group');
+editingButtonGroup.addEventListener('click', (e)=>{
+  let num;
+  if (e.target.nodeName === 'BUTTON') {
+    console.log(e.target.id);
+    num = (e.target.id === 'rotate-left') ? 1 :
+    (e.target.id === 'rotate-right') ? 2 :
+    (e.target.id === 'ratio-sqaure') ? 3 :
+    (e.target.id === 'ratio-rec') ? 4 :
+    (e.target.id === 'ratio-free') ? 5 :
+    0;
+    
+    switch (num) {
+      case 1:
+        cropper.rotate(-90);
+        console.log('21');
+        break;
+      case 2:
+        cropper.rotate(90);
+        break;
+      case 3:
+        cropper.setAspectRatio(1);
+        break;
+      case 4:
+        cropper.setAspectRatio(16/9);
+        break;
+      case 5:
+        cropper.setAspectRatio(NaN);
+        break;
+      default:
+        break;
+    }
+  }
+})
 // uploadImg
 const uploadbtn = document.querySelector('.input-file-button');
 const uploadinput = document.querySelector('.input-file-button-origin');
@@ -35,7 +69,10 @@ uploadinput.addEventListener('change',function() {
      recanvas.width = img.width;
      recanvas.getContext("2d").drawImage(img, 0, 0, img.width, img.height);
      //製作裁切版
-     cropper = new Cropper(recanvas);
+     cropper = new Cropper(recanvas,{
+      aspectRatio: 16 / 9,
+      viewMode: 2,
+     });
     
      //解放記憶體(?
      URL.revokeObjectURL(uploadimg.src)
@@ -46,6 +83,7 @@ uploadinput.addEventListener('change',function() {
     img.src =  URL.createObjectURL(this.files[0]);
     uploadimg.src = URL.createObjectURL(this.files[0]);
 
+    // 畫面的顯示隱藏
     uploadbtn.classList.add('hidden');
     if( Array.from(uploadimg.classList).indexOf('hidden') >= 0) {
         uploadimg.classList.remove('hidden');
@@ -62,7 +100,4 @@ reuploadbtn.addEventListener('click', function() {
 })
 
 //cropper 裁切版
-let cropper = new Cropper(recanvas, {
-  aspectRatio: 16 / 9,
-  viewMode: 2,
- });
+let cropper = new Cropper(recanvas);
